e9c124f5a67f661e0acb118bf8de924efef4aca5
Edgardo Obregón <edraobdu@gmail.com>
2020-03-21 00:45:46 -0500

    changed name of database for production

--- placesapi/settings.py
+++ placesapi/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'placesapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bezurnet_places',
+        'NAME': 'bezurnet_placesapi',
         'PORT': '',
         'HOST': 'localhost',
         'USER': 'bezurnet_places',
